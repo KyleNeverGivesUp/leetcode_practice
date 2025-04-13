0dfe1c5811bf490bb1b1cc460a9183f7151c072a
KyleAtASU <shunjieh@asu.edu>
2025-04-13 00:54:58 -0700

    first commit

---
+++ CMake/_bbuild/CMakeFiles/app.dir/add.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/app.dir/add.cpp.o: \
+  /Users/kyle/leetcode_practice/leetcode_practice/CMake/add.cpp
---
+++ CMake/substract.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// #include "head.h"
+
+int substract(int c, int b){
+    printf("sb");
+    return c - b;
+    
+}
